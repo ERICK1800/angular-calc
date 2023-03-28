9bca1b9c616291bbdf4a89fe4579896883067f23
ERICK1800 <ericklimaflorencio@gmail.com>
2023-03-28 17:46:18 -0300

    Second commit

--- src/app/calc/calc.component.ts
+++ src/app/calc/calc.component.ts
@@ -9,8 +9,20 @@ export class CalcComponent {
   num1 : number = 0;
   num2 : number = 0;
   soma : number = 0;
-  
+
   somar(){
     this.soma = this.num1 + this.num2;
+  }
+
+  subitrair(){
+    this.soma = this.num1 - this.num2;
+  }
+
+  multiplicar(){
+    this.soma = this.num1 * this.num2;
+  }
+
+  dividir(){
+    this.soma = this.num1 / this.num2;
   }
 }
